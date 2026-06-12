939795a7d396249c461b7da2eb82957ecd776b9d
Mhandisi <engsephy@gmail.com>
2026-06-12 11:50:30 +0300

    fix(website): prevent database crash during installation by deferring navigation queries

--- plugins/webkul/website/src/WebsitePlugin.php
+++ plugins/webkul/website/src/WebsitePlugin.php
@@ -9,6 +9,7 @@ use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\View\PanelsRenderHook;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Schema;
 use Webkul\PluginManager\Package;
 use Webkul\Website\Filament\Admin\Clusters\Settings\Pages\ManageContacts;
 use Webkul\Website\Filament\Customer\Auth\Login;
@@ -66,7 +67,7 @@ class WebsitePlugin implements Plugin
                             ->sort(2)
                             ->visible(fn (): bool => (bool) count(Account::getClusteredComponents())),
                     ])
-                    ->navigationItems($this->getNavigationItems()->toArray())
+                    ->navigationItems(fn () => $this->getNavigationItems()->toArray())
                     ->renderHook(
                         PanelsRenderHook::TOPBAR_END,
                         fn (): string => view('website::filament.customer.header.auth-links', [
@@ -134,6 +135,10 @@ class WebsitePlugin implements Plugin
     {
         $navigationItems = new Collection;
 
+        if (! Schema::hasTable('website_pages')) {
+            return $navigationItems;
+        }
+
         $pages = Page::where([
             'is_header_visible' => true,
             'is_published'      => true,
@@ -164,6 +169,10 @@ class WebsitePlugin implements Plugin
                 ->label(fn () => __('website::filament/app.navigation.footer.home'))
                 ->url('/'),
         ]);
+
+        if (! Schema::hasTable('website_pages')) {
+            return $navigationItems;
+        }
 
         $pages = Page::where([
             'is_footer_visible' => true,
